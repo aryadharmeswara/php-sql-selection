8724b8fc46647d5f7e9f74d72063adc1b5e0bc9f
aryadharmeswara <97155450+aryadharmeswara@users.noreply.github.com>
2023-11-20 10:08:57 +0800

    Add files via upload

---
+++ P10 sql/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020013_arya_p10";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+
+?>
